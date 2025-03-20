953146b5c272b91af122b55e340bd4ba12dbc4a9
raulportales <raul.portales@fyware.com>
2025-03-20 16:15:23 -0600

    added base test file

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+"""test_main.py - test cases for the main module."""
+from fastapi.testclient import TestClient
+from yt_clone.main import app
+
+client = TestClient(app)
+
+def test_health_check():
+    """ Test the health check endpoint. """
+    response = client.get("/HealthCheck")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
