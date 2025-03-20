b5b7cbfd0082046df60b3c9a4a95ad54bc8214c0
raulportales <raul.portales@fyware.com>
2025-03-20 14:06:26 -0600

    Initial tree with sample file upload

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-from enum import Enum
-
-from fastapi import FastAPI
-
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
+from fastapi import Depends, FastAPI
+from .routers import files
 
 app = FastAPI()
 
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+app.include_router(files.router)
 
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
---
+++ routers/files.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter,File, UploadFile
+from typing import Annotated
+from fastapi.responses import HTMLResponse
+
+
+router = APIRouter();
+
+
+
+@router.post("/files/")
+async def create_files(
+    files: Annotated[list[bytes], File(description="Multiple files as bytes")],
+):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@router.post("/uploadfiles/")
+async def create_upload_files(
+    files: Annotated[
+        list[UploadFile], File(description="Multiple files as UploadFile")
+    ],
+):
+    return {"filenames": [file.filename for file in files]}
+
+
+@router.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
