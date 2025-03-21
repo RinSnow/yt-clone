84fde7e19736d2db9079020a194b05da74ba0c02
Rin <drakoscream@gmail.com>
2025-03-21 00:31:33 -0600

    ruff formatting

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-""" Main file for the FastAPI application. """
+""" "Main module for the FastAPI application."""
+
 from fastapi import FastAPI
 from .routers import files
 
 app = FastAPI()
 
+
 @app.get("/HealthCheck")
 def health_check():
-    """ Health check endpoint. """
+    """Health check endpoint."""
     return {"status": "ok"}
+
 
 app.include_router(files.router)
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,11 +1,13 @@
-"""test_main.py - test cases for the main module."""
-from fastapi.testclient import TestClient
+"""main.py test cases."""
+
 from app.main import app
+from fastapi.testclient import TestClient
+
 
 client = TestClient(app)
 
+
 def test_health_check():
-    """ Test the health check endpoint. """
     response = client.get("/HealthCheck")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
