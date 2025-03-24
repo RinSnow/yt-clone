afa41fc063a12dc27633ea5f89d81fddd849a692
Rin <drakoscream@gmail.com>
2025-03-24 01:49:47 -0600

    moved get settings function to config

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 """ "Main module for the FastAPI application."""
 
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
+from typing import Annotated
 from .src.routers import files
 from .src.database import engine, Base
+from .src.config import Settings,get_settings
+
 
 app = FastAPI()
 Base.metadata.create_all(engine)
@@ -12,5 +15,11 @@ def health_check():
     """Health check endpoint."""
     return {"status": "ok"}
 
+@app.get("/info")
+async def info(settings: Annotated[Settings, Depends(get_settings)]):
+    return {
+        "app_name": settings.app_name,
+        "admin_email": settings.admin_email
+    }
 
 app.include_router(files.router)
