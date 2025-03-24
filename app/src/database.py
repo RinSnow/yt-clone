5ce2f676b57ff0c8dce68002a1f2620d9cad1453
Rin <drakoscream@gmail.com>
2025-03-24 01:54:06 -0600

    Changed hardcode values for env variables

--- app/src/database.py
+++ app/src/database.py
@@ -1,13 +1,15 @@
-import os
+"""This file contains the database configuration and the database session."""
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-
+from .config import get_settings
 ###
 # Database Configuration
 ###
 
-SQLALCHEMY_DATABASE_URL = "postgresql://rin:fastapi@localhost:5432/yt-clone"
+db_settings = get_settings()
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_settings.db_user}:{db_settings.db_pass}@{db_settings.db_host}:5432/yt-clone"
+print(db_settings.url_test)
 connect_args = {"check_same_thread": False}
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
